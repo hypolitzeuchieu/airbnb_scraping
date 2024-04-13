dc741f5004a0e5d0d310c1f5851f5e59d56d5855
Hypolit <hypolitzeuchieu@gmail.com>
2024-04-13 13:00:41 +0100

    updating function to get price on page

--- airbnb.py
+++ airbnb.py
@@ -24,20 +24,25 @@ class AirbnbScraper:
         price_list = []
         self.driver.get(url)
         try:
-            prices = self.driver.find_elements(By.CLASS_NAME, "_1jo4hgw")
+            prices = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "_1jo4hgw"))
+            )
             if prices:
-                for price in prices:
-                    last_price = price.find_element(By.CLASS_NAME, "_1y74zjx") or price.find_element(By.CLASS_NAME,
-                                                                                                     "_1ks8cgb")
-                    price_digit = re.sub(r'\D', '', last_price.text)
-                    if price_digit.isdigit():
-                        price_list.append(int(price_digit))
-                        time.sleep(random.uniform(8, 10))
-                average = sum(price_list) / len(price_list) if len(price_list) else 0
+                try:
+                    for price in prices:
+                        last_price = price.find_element(By.CLASS_NAME, "_1y74zjx") or price.find_element(By.CLASS_NAME,
+                                                                                                         "_1ks8cgb")
+                        price_digit = re.sub(r'\D', '', last_price.text)
+                        if price_digit.isdigit():
+                            price_list.append(float(price_digit))
+                except (NoSuchElementException, AttributeError):
+                    self.logger.warning(f"Error extracting price from element:{price}")
+
+                average = float(sum(price_list) / len(price_list)) if len(price_list) else 0
                 return average
 
-        except NoSuchElementException as e:
-            self.logger.warning(f"something went wront to fetch the content from url:{e}")
+        except (NoSuchElementException, AttributeError) as e:
+            self.logger.warning(f"something went wront to fetch the content on {url}:{e}")
             return 0
 
     def get_next_page_button(self, url: str) -> bool:
@@ -67,8 +72,8 @@ class AirbnbScraper:
 
 
 if __name__ == "__main__":
-    url = "https://fr.airbnb.com/s/Rio-de-Janeiro--Rio-de-Janeiro--Br%C3%A9sil/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2024-05-01&monthly_length=3&monthly_end_date=2024-08-01&price_filter_input_type=0&channel=EXPLORE&query=Rio%20de%20Janeiro%2C%20Br%C3%A9sil&date_picker_type=calendar&checkin=2024-04-28&checkout=2024-05-26&source=structured_search_input_header&search_type=filter_change&price_filter_num_nights=28&place_id=ChIJW6AIkVXemwARTtIvZ2xC3FA&search_mode=regular_search&adults=2&children=1"
+    url1 = "https://fr.airbnb.com/"
     scraper = AirbnbScraper()
-    price1 = scraper.get_next_page_button(url)
+    price1 = scraper.get_average_price(url1)
     print(price1)
     scraper.close()

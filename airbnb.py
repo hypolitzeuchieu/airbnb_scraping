fcada55f92a1947f4e80199868bfdf6e040a5fbb
Hypolit <hypolitzeuchieu@gmail.com>
2024-05-26 11:30:15 +0100

    get the price of interval vacation

--- airbnb.py
+++ airbnb.py
@@ -1,15 +1,16 @@
-import time
+import logging
 import random
+import re
+import time
+from datetime import datetime
+
 
 from selenium import webdriver
-import re
-import logging
-
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementNotInteractableException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class AirbnbScraper:
@@ -46,12 +47,13 @@ class AirbnbScraper:
             self.logger.warning(f"something went wrong to fetch the content on {url}:{e}")
             return 0
 
-    def get_next_page_button(self, url: str) -> bool:
+    def get_next_page_button(self, url: str):
         self.driver.get(url)
         page_number = 1
+        url_list = []
         try:
             while True:
-                print(f"we are on page:{page_number}")
+                print(f'we are {page_number}')
                 next_button = WebDriverWait(self.driver, 10).until(
                     EC.presence_of_element_located((By.CSS_SELECTOR, ".c1ytbx3a"))
                 )
@@ -59,11 +61,11 @@ class AirbnbScraper:
                 if next_button.get_attribute("aria-disabled") == "true":
                     print("it's the last page")
                     break
-
                 page_number += 1
                 next_button.click()
+                url_list.append(self.driver.current_url)
                 time.sleep(random.uniform(3, 7))
-
+            return url_list
         except NoSuchElementException:
             print("No button on this page")
             return False
@@ -72,9 +74,11 @@ class AirbnbScraper:
             self.logger.error(f" next button click error:{e}")
             return False
 
-    def get_holiday_country(self, url: str, country_name: str):
+    def navigate_to_month(self, url: str, country_name: str, begin_day: datetime, end_day: datetime):
         self.driver.get(url)
         wait = WebDriverWait(self.driver, 10)
+
+        # try to close the pub button if exist
         try:
             # to close pub
             button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[aria-label='Fermer']")))
@@ -84,23 +88,61 @@ class AirbnbScraper:
         except TimeoutException:
             print("Close cookies button doesn't exist on page  ")
 
-        country_input = self.driver.find_element(By.ID, "bigsearch-query-location-input")
+        # Enter the name of the city where you are going
+        country_input = wait.until(EC.element_to_be_clickable((By.ID, "bigsearch-query-location-input")))
         country_input.clear()
         country_input.send_keys(country_name)
 
+        # click on the first suggestion
         first_suggestion = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.smfh8a4")))
         first_suggestion.click()
 
-        month = self.driver.find_element(By.ID, "tab--tabs--1")
-        month.click()
+        # select the input month
+        try:
+            # month = self.driver.find_element(By.CLASS_NAME, "lk4ruxu")
+            # month.click()
+            month_flexible = wait.until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="search-tabpanel"]/div/div[3]/div[1]/div/div/div[2]')))
+            month_flexible.click()
+        except TimeoutException as e:
+            raise e
 
-        element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "d1u68d5p")))
-        element.click()
+        # select the interval of vacation date
+        while True:
+            try:
+                next_month_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                                                           '[aria-label="Avancez pour passer au mois suivant."]')))
+                try:
+                    begin_day_selector = f'[data-testid="calendar-day-{begin_day.strftime("%d/%m/%Y")}"]'
 
+                    begin_day_element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, begin_day_selector)))
+                    if begin_day_element:
+                        begin_day_element.click()
+                        while True:
+                            try:
+                                end_day_selector = f'[data-testid="calendar-day-{end_day.strftime("%d/%m/%Y")}"]'
+                                end_day_element = wait.until(
+                                    EC.element_to_be_clickable((By.CSS_SELECTOR, end_day_selector)))
+                                if end_day_element:
+                                    end_day_element.click()
+                                    break
+                            except:
+                                next_month_button.click()
+                        break
+                except:
+                    next_month_button.click()
+                    print('we are going to pass to the next month')
+
+            except ElementNotInteractableException as er:
+                print(f'error to interactable :{er}')
+                raise er
+            except Exception as e:
+                print(f'Something went wrong: {e}')
+                raise e
+        # add the number of travellers
         travelers = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
                                                            '[data-testid="structured-search-input-field-guests-button"]')))
         travelers.click()
-        # button to add travelers
         adults = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
                                                         '[data-testid="stepper-adults-increase-button"]')))
         adults.click()
@@ -110,10 +152,12 @@ class AirbnbScraper:
                                                         '[data-testid="stepper-children-increase-button"]')))
         childs.click()
 
+        # click on the search button
         search_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
                                                                '[data-testid="structured-search-input-search-button"]')))
         search_button.click()
-        return search_button
+
+        return self.driver.current_url
 
     def close(self):
         self.driver.quit()
@@ -122,6 +166,16 @@ class AirbnbScraper:
 if __name__ == "__main__":
     url1 = "https://fr.airbnb.com/"
     scraper = AirbnbScraper()
-    price1 = scraper.get_holiday_country(url1, "pari")
-    price2 = scraper.get_average_price(price1)
+    begin_vacation_day = datetime(2024, 6, 28)
+    end_vacation_day = datetime(2024, 7, 12)
+    country_date = scraper.navigate_to_month(url1, 'rio', begin_day=begin_vacation_day, end_day=end_vacation_day)
+    all_pages = scraper.get_next_page_button(country_date)
+    prices = []
+    for page in all_pages:
+        price1 = scraper.get_average_price(page)
+        prices.append(price1)
+    print(sum(prices)/len(prices))
+    # average_price = scraper.get_next_page_button(
+    #     'https://fr.airbnb.com/s/Europe/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2024-06-01&monthly_length=3&monthly_end_date=2024-09-01&price_filter_input_type=0&channel=EXPLORE&place_id=ChIJhdqtz4aI7UYRefD8s-aZ73I&date_picker_type=calendar&source=structured_search_input_header&search_type=filter_change'
+    # )
     # scraper.close()
